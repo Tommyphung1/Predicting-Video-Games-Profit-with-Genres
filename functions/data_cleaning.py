c249dd8c022b1871ef618cdb48d18952085cbf50
Tommyphung1 <117569251+Tommyphung1@users.noreply.github.com>
2023-07-17 01:20:37 -0700

    More exploratioin and some visuals

---
+++ functions/data_cleaning.py
@@ -0,0 +1,9 @@
+def value_to_float(x):
+    if type(x) == float or type(x) == int:
+        return x
+    if 'K' in x:
+        if len(x) > 1:
+            return float(x.replace('K', '')) * 1000
+        return 1000
+    return 0.0
+    
